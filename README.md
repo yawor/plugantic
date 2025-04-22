274783dde26096ccbe2a067941e9aa1b3e439519
Marcin Jaworski <marcin@jaworski.me>
2025-04-22 21:32:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,94 @@
+# plugantic
+
+A [pydantic](https://github.com/pydantic/pydantic) based plugin loading and configuration.
+
+This library provides classes, which can be used in pydantic based application configuration to load and configure
+plugin instances, which are then available directly from the configuration model instance.
+
+The library uses python's entry points to define available plugins.
+
+The `BasePlugin` class provided in the library doesn't provide any implementation specific to how the application should
+use the plugins. It only implements a mechanisms for lookup, loading and configuring the plugins' instances.
+It's up to the application itself to implement application-specific interfaces.
+
+## Example
+
+This code defines base plugin (`BaseApplicationPlugin`) and plugin config model (`BaseApplicationPluginConfig`) classes
+specific for the application. The `BaseApplicationPlugin` defines that it uses `BaseApplicationPluginConfig` as its
+config model. Any plugin implementation that inherits from `BaseApplicationPlugin` must use a config model which is
+either `BaseApplicationPluginConfig` or inherits from it. It is checked in the runtime, and validation errors are raised
+if this is not true.
+
+It also defines an application config model class, which has `plugins` field, which holds a list of plugin instances.
+The plugins are going to be looked up in `myapp.plugin` entry point group. The config model expects that all plugins
+in this field have to inherit from `BaseApplicationPlugin`.
+
+```python
+from abc import ABC
+from typing import Annotated
+
+from yaml import safe_load
+from pydantic import BaseModel
+from plugantic import PluginAnnotation, BasePlugin, BasePluginConfig
+
+
+class BaseApplicationPluginConfig(BasePluginConfig):
+  x: int = 0
+
+
+class BaseApplicationPlugin(BasePlugin, ABC):
+  _config: BaseApplicationPluginConfig
+
+  def application_specific_method(self):
+    ...
+
+
+class ApplicationConfig(BaseModel):
+  plugins: list[Annotated[BaseApplicationPlugin, PluginAnnotation("myapp.plugin")]]
+
+
+def main():
+  with open("config.yaml") as config_file:
+    config = ApplicationConfig.model_validate(safe_load(config_file))
+
+  print(config.plugins)
+```
+
+An example configuration file for the above code could look like this:
+
+```yaml
+plugins:
+  - name: cool_plugin
+    x: 10
+  - other_plugin
+```
+
+It defines two plugins:
+
+- The first one is named `cool_plugin`. It also sets the `x` parameter in the plugin's config
+  to 10.
+- The second one is named `other_plugin` and it uses a shorthand configuration form, where only a name is provided
+  instead a full dictionary with a `name` key. It is possible to use shorthand form only if the plugin doesn't
+  take any configuration parameters or if default values are provided in the plugin's config model like in the example
+  above.
+
+This is equivalent to:
+
+```yaml
+plugins:
+  - name: cool_plugin
+    x: 10
+  - name: other_plugin
+```
+
+The plugins themselves must be provided by registering entry points for requested entry point groups. An example
+definition of a plugin in `pyproject.toml` in a package providing the plugins may look like this:
+
+```toml
+[project.entry-points."myapp.plugin"]
+cool_plugin = "myapp_plugins.cool_pluging:CoolPlugin"
+other_plugin = "myapp_plugins.other_plugin:OtherPlugin"
+```
+
+The entry point needs to point to a class which inherits from the `PluginAnnotation` annotated class in the config
+model.
---
+++ src/plugantic/__init__.py
@@ -0,0 +1,129 @@
+import dataclasses
+from abc import ABC
+from typing import Any, get_type_hints
+from importlib.metadata import entry_points
+
+from pydantic import BaseModel, ConfigDict, GetCoreSchemaHandler
+from pydantic_core import core_schema
+
+
+class BasePluginConfig(BaseModel):
+    """
+    A base class for plugin configuration. If a plugin needs specific configuration parameters, a new class should
+    be created, which inherits from this one, and it should include fields for the parameters.
+    """
+
+    model_config = ConfigDict(extra="allow")
+
+    name: str
+    """
+    This field indicated a plugin name, which is a name of the entry point that points to the plugin's class.
+    """
+
+
+class BasePlugin(ABC):
+    """
+    A base class for plugins. The application should create its own base classes for specific types of plugins, which
+    are then inherited by plugin implementations.
+    """
+
+    _config: BasePluginConfig
+    """
+    A type annotation for configuration model's class for the plugin.
+    """
+
+    def __init__(self, config: BasePluginConfig):
+        self._config = config
+
+
+@dataclasses.dataclass(frozen=True)
+class PluginAnnotation:
+    """
+    A metadata class to annotate a model field as a plugin instance. It takes a `group_name` argument, which is
+    a name of the entry points group for plugin lookup.
+
+    Example::
+
+        from typing import Annotated
+        from pydantic import BaseModel
+        from plugantic import BasePlugin, PluginAnnotation
+
+        class Config(BaseModel):
+            plugin: Annotated[BasePlugin, PluginAnnotation("app.plugin")]
+
+        c = Config.model_validate({"plugin": {"name": "myplugin"}})
+
+    This tries to load a plugin class from entry point named `myplugin` in `app.plugin` group.
+
+    If plugin doesn't require any configuration parameters to be provided (either doesn't have any or
+    provides default values), it is possible to use the shorthand version::
+
+        c = Config.model_validate({"plugin": "myplugin"})
+
+    """
+
+    plugin_group: str
+    """A name of an entry point group for annotated plugin"""
+
+    def __get_pydantic_core_schema__(
+        self, source_type: Any, handler: GetCoreSchemaHandler
+    ) -> core_schema.CoreSchema:
+        if not issubclass(source_type, BasePlugin):
+            raise RuntimeError(f"{source_type} is not a subclass of {BasePlugin}")
+
+        base_config_class = get_type_hints(source_type)["_config"]
+
+        def validate_from_config(value: str | BasePluginConfig) -> BasePlugin:
+            if not isinstance(value, BasePluginConfig):
+                value = BasePluginConfig.model_validate({"name": value})
+
+            eps = entry_points(name=value.name, group=self.plugin_group)
+            try:
+                (ep,) = eps
+            except ValueError:
+                raise ValueError(
+                    f"Can't find entry point {value.name} for group {self.plugin_group}"
+                )
+
+            try:
+                plugin_class = ep.load()
+            except Exception as e:
+                raise ValueError(
+                    f"Can't load entry point {value.name} for group {self.plugin_group}"
+                ) from e
+
+            if not issubclass(plugin_class, source_type):
+                raise ValueError(f"{plugin_class} is not a subclass of {source_type}")
+
+            plugin_config_class = get_type_hints(plugin_class)["_config"]
+            if not issubclass(plugin_config_class, BasePluginConfig):
+                raise ValueError(
+                    f"{plugin_config_class} is not a subclass of {base_config_class}"
+                )
+
+            config = plugin_config_class.model_validate(value.model_dump())
+
+            p = source_type(config)
+            return p
+
+        from_plugin_config_schema = core_schema.chain_schema(
+            [
+                core_schema.union_schema(
+                    [
+                        core_schema.str_schema(min_length=1),
+                        base_config_class.__pydantic_core_schema__,
+                    ]
+                ),
+                core_schema.no_info_plain_validator_function(validate_from_config),
+            ]
+        )
+
+        return core_schema.json_or_python_schema(
+            json_schema=from_plugin_config_schema,
+            python_schema=core_schema.union_schema(
+                [
+                    core_schema.is_instance_schema(BasePlugin),
+                    from_plugin_config_schema,
+                ]
+            ),
+        )
